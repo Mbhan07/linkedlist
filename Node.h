6143f2a09843816474c6eb8b3f078e101bb54a73
U-BSD\419503 <419503@SHS-C5W2NW3.beaverton.k12.or.us>
2024-12-03 09:55:47 -0800

    adding Node.h. some errors to fix

---
+++ Node.h
@@ -0,0 +1,20 @@
+#ifndef NODE_H //beginning of header guard
+#define NODE_H
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Node{
+ public:
+  Node();
+  ~Node();
+  void setValue(int newvalue);
+  int getValue();
+  void setNext(Node* newnext);
+  Node* getNext()'
+ private:
+  int value;
+  Node* next;
+};
+#endif
