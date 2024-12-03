efab40ef8b7b26e77b669c7a7ba91779fb19e83a
U-BSD\419503 <419503@SHS-C5W2NW3.beaverton.k12.or.us>
2024-12-03 09:56:20 -0800

    adding main.cpp - need to fix error

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+/*
+
+  This is the main file for Linked Lists, aproject that explores how objects can be stored, and retrieved using successive pointers. This is another project also dealing with inheritance.
+
+  By: Mahika Bhan
+  12/03/2024
+
+
+ */
+
+void add(int newvalue);
+void print(Node* next);
+
+int main(){
+  Node* head = NULL;
+  add(5);
+  print(head);
+  add(7)
+  print(head);
+  add(2);
+  print(head);
+
+  return 0;
+}
+
+void add(int newvalue) {
+  Node* current = head;
+
+  if (current == NULL) {
+    head = new Node();
+    head->setValue(newvalue);
+  }else {
+    while (current->getNext() != NULL){
+      current = current ->getNext();
+    }
+    current ->setNext(new Node());
+    current ->getNext()->setValue(newvalue);
+
+  }
+}
+
+void print(Node* next){
+  if(next == head){
+    cout << "lists:";
+  }
+  if(next != NULL){
+    cout << next->getValue() << " ";
+    print(next->getNext());
+  }
+}
