812a91394c5780b7c54400e2a9e62a837c88396e
U-BSD\419503 <419503@SHS-C5W2NW3.beaverton.k12.or.us>
2024-12-10 10:09:58 -0800

    updated

--- Student.cpp
+++ Student.cpp
@@ -3,3 +3,38 @@
 #include "Student.h"
 
 using namespace std;
+
+/*
+
+  Mahika Bhan, 12/10/2024
+
+  This is the student.cpp file
+ */
+
+
+Student::Student(char* firstNameInput, char* lastNameInput, int inputid, float inputgpa){
+  firstname = new char[200];
+  lastname = new char[200];
+  strcpy(firstname, firstNameInput);
+  strcpy(lastname, lastNameInput);
+  id = inputid;
+  gpa = inputgpa
+}
+
+
+char * Student::getFirstName(){
+  return firstname;
+}
+
+char* Student::getLastName(){
+  return lastname;
+}
+
+int Student::getID(){
+
+  return id;
+}
+
+float Student::getGPA(){
+  return gpa;
+}
