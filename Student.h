e19cc92ed3aae4c4e04ba456e99b4fb51d9115ac
Mahika Bhan <bhan.mahika813@gmail.com>
2024-12-11 21:37:51 -0800

    updated student.h

--- Student.h
+++ Student.h
@@ -10,5 +10,16 @@ using namespace std;
 
 class Student{
   public;
-  
+  Student();
+  Student(char* firstNameInput, char * lastNameInput, int inputid, float inputgpa);
+  char * getFirstName();
+  char * getLastName();
+  int getID();
+  float getGPA();
+  char * firstname;
+  char * lastname;
+  int id;
+  int gpa;
 };
+
+#endif
