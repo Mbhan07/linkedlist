65572e1523c5c2e180f0be40b23da2b1e085c0ee
Mahika Bhan <bhan.mahika813@gmail.com>
2024-12-15 23:15:34 -0800

    adding comments

--- Node.h
+++ Node.h
@@ -5,17 +5,36 @@
 #include <cstring>
 
 using namespace std;
+/*
+Contains all the variables/functions for the Node.cpp file and defines the NOde class
 
+Mahika Bhan, 12/13/2024
+
+ */
 class Node{
  public:
+  //constructor
   Node(Student * newStudent);
+
+  //destructor: cleanrs up memory allocated for NOde
   ~Node();
+
+  //sets student associatedf with node to nerw student object
   void setStudent(Student* newStudent);
+
+  //returns first name of student stored in node
   char* getStudent();
   void setNext(Node* newnext);
   Node* getNext();
  private:
+
+  //pointer to next student object
   Student* value;
+
+  //pointer to next node in linked list
   Node* next;
+  
 };
+
+//end of header guard
 #endif
